826492a9eabd75e4abcb3a81397f76a33e589835
Lóránt Pintér <lorant.pinter@gmail.com>
2025-01-15 20:19:22 +0100

    Init NVS

--- main/main.cpp
+++ main/main.cpp
@@ -8,6 +8,7 @@
 #include <esp_log.h>
 #include <esp_pm.h>
 #include <esp_sleep.h>
+#include <nvs_flash.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
@@ -23,6 +24,14 @@ esp_pm_lock_handle_t noSleep;
 static const gpio_num_t ledGpio = GPIO_NUM_23;
 
 extern "C" void app_main() {
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
     // Set up GPIO for the LED
     gpio_config_t ledConfig = {
         .pin_bit_mask = 1ULL << ledGpio,
